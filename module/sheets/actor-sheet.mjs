3bceb2e6df6e7010df3133abe03424357265238a
Luiz Bertoni <luiz.rcb@gmail.com>
2026-09-03 13:34:06 -0300

    fix: :zap: Improved Compendium Browser performance

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -1152,7 +1152,7 @@ export class SdmActorSheet extends api.HandlebarsApplicationMixin(sheets.ActorSh
     event.stopPropagation();
     if (event.detail > 1) return; // Ignore repeated clicks
 
-    return new CompendiumBrowser().render(true);
+    return CompendiumBrowser.open();
   }
 
   static async _onRerollCharacter(event, target) {
